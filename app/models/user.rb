9dd9f46ae4ac4399f80eaf50467e2fd57c0aebbe
kazuhikomiyamoto <k.miyamoto.0128@gmail.com>
2015-09-24 15:12:36 +0000

    want have

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,42 +9,64 @@ class User < ActiveRecord::Base
   
   has_many :following_relationships, class_name:  "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :following_users, through: :following_relationships, source: :followed
+  
   has_many :followed_relationships, class_name:  "Relationship", foreign_key: "followed_id", dependent: :destroy
   has_many :followed_users, through: :followed_relationships, source: :follower
 
   has_many :ownerships , foreign_key: "user_id", dependent: :destroy
   has_many :items ,through: :ownerships
-
-
+  
+  has_many :wants, class_name: "Want", foreign_key: "user_id", dependent: :destroy
+  has_many :want_items , through: :wants, source: :item
+  
+  has_many :haves, class_name: "Have", foreign_key: "user_id", dependent: :destroy
+  has_many :have_items , through: :haves, source: :item
+  
   # 他のユーザーをフォローする
   def follow(other_user)
     following_relationships.create(followed_id: other_user.id)
   end
 
+  # フォローしているユーザーをアンフォローする
   def unfollow(other_user)
     following_relationships.find_by(followed_id: other_user.id).destroy
   end
-
+  
+  # あるユーザーをフォローしているかどうか？
   def following?(other_user)
     following_users.include?(other_user)
   end
 
   ## TODO 実装
+  
+  # あるアイテムをHaveする
   def have(item)
+    haves.find_or_create_by(item_id: item.id)
   end
-
+  
+  # HaveしてあるアイテムをUnHaveする　
   def unhave(item)
+    haves.find_by(item_id: item.id).destroy
   end
-
+  
+  # あるアイテムをHaveしてあるか？
   def have?(item)
+    have_items.include?(item)
   end
-
+  
+  # あるアイテムをWantする
   def want(item)
+    wants.find_or_create_by(item_id: item.id)
   end
-
+  
+  # WantしてあるアイテムをUnwantする
   def unwant(item)
+    wants.find_by(item_id: item.id).destroy
   end
 
+  # あるアイテムをWantしてあるか？
   def want?(item)
+    want_items.include?(item)
   end
+  
 end
